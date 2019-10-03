1218718c92e9d73e130389a853d4d75871752425
marcelotakayama <marcelo.takayama00@gmail.com>
2019-10-03 19:06:52 -0300

    Alterações e implementações 55:04

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,16 @@
 // Importação do express
 const express = require ('express');
+const mongoose = require('mongoose');
 
-// Estância da aplicação
+const routes = require('./routes');
+
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@clusterapi-zoq3q.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 
 
 // Modelo de requisição que pode ser feita de diversos modelos:
@@ -17,10 +25,11 @@ const app = express();
 // Se você printa os bytecodes na hora de executar o arquivo em node, o programa irá 
 // Mostrar os bytecodes com mais detalhes
 
-app.get('/users', (req, res) => {
-    
-    res.json({ id: req.params.id });
-});
+app.use(express.json());
+app.use(routes);
+
+
+
 
 // Porta em que a aplicação está sendo ouvida
 app.listen(3333);
